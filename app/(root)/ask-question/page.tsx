eb3d84d95c999642af63e657f7a788fa73cbb915
Yashpreetrana4790 <109335257+Yashpreetrana4790@users.noreply.github.com>
2023-11-13 23:10:13 +0530

    refreshed the data env and all

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,14 +1,13 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const page = async () => {
-  // const { userId } = auth();
-  const userId = "12345";
+  const { userId } = auth();
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
-  console.log(mongoUser);
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
